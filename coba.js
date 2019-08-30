b040449078ddd584ff24134df5a6b7c33cd2c965
adityain98 <adityain98@gmail.com>
2019-08-30 20:09:41 +0700

    perbaikan 16

--- coba.js
+++ coba.js
@@ -1,6 +1,75 @@
-var halo = {
-    satu: {nama: 'indra'},
-    dua: {nama: 'hasil'}
+// function sorting(arrNumber) {
+//     for(var i = 0; i<arrNumber.length; i++){
+//         for(var j = i+1; j<arrNumber.length; j++){
+//             if(i < j){
+//                 var pindah = arrNumber[i];
+//                 arrNumber[i] = arrNumber[j];
+//                 arrNumber[j] = pindah;
+//             }
+//         }
+//     }
+//     return arrNumber;
+//     }
+  
+// function getTotal(arrNumber) {
+//     var hasil = [];
+//     var jumlah = [];
+//     for(var i = 0; i <arrNumber.length; i++){
+//         if(i == 0){
+//             hasil.push(arrNumber[i]);
+//             jumlah.push(1);
+//         }
+//         else{
+//             var isDuplicate = false;
+//             for(var j = 0; j<hasil.length; j++){
+//                 if(arrNumber[i] == hasil[j]){
+//                     jumlah[j] += 1;
+//                     isDuplicate = true;
+//                 }
+//             }
+//             if(isDuplicate == false){
+//                 hasil.push(arrNumber[i]);
+//                 hasil.push(1);
+//             }
+//         }
+//     }
+//     return jumlah;
+// }
+  
+//   function mostFrequentLargestNumbers(arrNumber) {
+//     var listSort = sorting(arrNumber);
+//     var countHighest = getTotal(listSort);
+//     return listSort;
+//   }
+  
+//   console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+  
+//   console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//   //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+  
+//   console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//   //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+  
+//   console.log(mostFrequentLargestNumbers([]));
+//   //''
+
+var hasil = 0
+
+function coba(num, num2){
+
+    if(num2 == undefined){
+        num2 = 6;
+        hasil = 0;
+    }
+    
+    if(num2 == 0){
+        return hasil;
+    }
+    else{
+        hasil += 1;
+        return coba(num, num2 - 1);
+    }
 }
 
-console.log(halo.length);
+console.log(coba(5));
--- exercises-16.js
+++ exercises-16.js
@@ -1,129 +1,124 @@
-function graduates (students) {
-    var hasil = {};
-    var sementara = {};
-    var nama = [];
-    var kelas = [];
-    var nilai = [];
-    var jumlah = [];
+function isEmpty(obj){
+  for(var a in obj) {
+    if(obj.hasOwnProperty(a)){
+      return false;
+    }
+  }
+  return true;
+}
 
-    for(var i = 0; i<students.length; i++){
-      if(i == 0){
-        kelas.push(students[i].class);
-        if(students[i].score >= 75){
-          nama.push(students[i].name);
-          nilai.push(students[i].score);
-          jumlah.push(1);
-        }
-        else{
-          jumlah.push(0);
-        }
+function graduates (students) {
+  var kelas = [];
+  var nama = {};
+  var hasil = {};
+
+  for(var i = 0; i<students.length; i++){
+    nama = {};
+    kelas = [];
+    if(isEmpty(hasil) == true){
+      if(students[i].score > 74){
+        nama.name = students[i].name;
+        nama.score = students[i].score;
+        kelas.push(nama);
+        hasil[students[i].class] = kelas;
       }
-      else{
-        var sama = false;
-        for(var j = 0; j<kelas.length; j++){
-          if(students[i].class == kelas[j]){
-            sama = true;
-            if(students[i].score >= 75){
-              nama.push(students[i].name);
-              nilai.push(students[i].score);
-              jumlah[j] += 1
-            }
+    }
+    else{
+      var isDuplicate = false;
+      if(students[i].score > 74){
+        for(var a in hasil){
+          if(a == students[i].class){
+            nama.name = students[i].name;
+            nama.score = students[i].score;
+            hasil[a].push(nama);
+            isDuplicate = true;
           }
         }
-        if(sama == false){
-          kelas.push(students[i].class);
-            if(students[i].score >= 75){
-              nama.push(students[i].name);
-              nilai.push(students[i].score);
-              jumlah.push(1)
-            }
-            else{
-              jumlah.push(0)
-            }
+        if(isDuplicate == false){
+          nama.name = students[i].name;
+          nama.score = students[i].score;
+          kelas.push(nama);
+          hasil[students[i].class] = kelas;
         }
       }
     }
-    console.log(kelas);
-    console.log(nama);
-    console.log(jumlah);
-    return nilai
-    
-    
   }
-  
-  console.log(graduates([
-    {
-      name: 'Dimitri',
-      score: 90,
-      class: 'foxes'
-    },
-    {
-      name: 'Alexei',
-      score: 85,
-      class: 'wolves'
-    },
-    {
-      name: 'Sergei',
-      score: 74,
-      class: 'foxes'
-    },
-    {
-      name: 'Anastasia',
-      score: 78,
-      class: 'wolves'
-    }
-  ]));
-  
-  // {
-  //   foxes: [
-  //     { name: 'Dimitri', score: 90 }
-  //   ],
-  //   wolves: [
-  //     { name: 'Alexei' , score: 85 },
-  //     { name: 'Anastasia', score: 78 }
-  //   ]
-  // }
-  
-  console.log(graduates([
-    {
-      name: 'Alexander',
-      score: 100,
-      class: 'foxes'
-    },
-    {
-      name: 'Alisa',
-      score: 76,
-      class: 'wolves'
-    },
-    {
-      name: 'Vladimir',
-      score: 92,
-      class: 'foxes'
-    },
-    {
-      name: 'Albert',
-      score: 71,
-      class: 'wolves'
-    },
-    {
-      name: 'Viktor',
-      score: 80,
-      class: 'tigers'
-    }
-  ]));
-  
-  // {
-  //   foxes: [
-  //     { name: 'Alexander', score: 100 },
-  //     { name: 'Vladimir', score: 92 }
-  //   ],
-  //   wolves: [
-  //     { name: 'Alisa', score: 76 },
-  //   ],
-  //   tigers: [
-  //     { name: 'Viktor', score: 80 }
-  //   ]
-  // }
-  
-  
-  console.log(graduates([])); //{}
+  return hasil;
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
